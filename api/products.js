a9b4438a250cfea7528d0a5acafcfddd6858b659
Harsh Varshney <harshvarshney5074@gmail.com>
2025-12-17 15:34:08 +0530

    Working on admin merch section

--- api/products.js
+++ api/products.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import { ObjectId } from "mongodb";
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
