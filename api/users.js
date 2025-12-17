3c500376030e9eae9786bb2f1bec152ccd8331dc
Harsh Varshney <harshvarshney5074@gmail.com>
2025-12-17 18:21:17 +0530

    Working on admin merch section

--- api/users.js
+++ api/users.js
@@ -1,6 +1,9 @@
 // api/users.js
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_SERVICE_ROLE_KEY:", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
